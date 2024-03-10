76bd2df2afcebea7bb4bd846c0ec66d55b701144
ExtremeBlackLiu <extremeblackliu@gmail.com>
2024-03-11 07:28:19 +0800

    Update DirectLocalTest.bat

--- DirectLocalTest.bat
+++ DirectLocalTest.bat
@@ -5,7 +5,7 @@ echo [-VRChat-LocalTest-Launcher-]
 echo [-Author---Extr3lackLiu-----]
 echo [---------------------------]
 
-REM string randomDigits = Tools.GetRandomDigits(10); lol
+REM string randomDigits = Tools.GetRandomDigits(10);
 REM string text4 = "--url=create?roomId=" + randomDigits + "&hidden=true&name=BuildAndRun&url=file:///" + text;
 set /a "randomid_a=10000+%random%"
 set /a "randomid_b=10000+%random%"
@@ -15,6 +15,11 @@ dir /s /b *.vrcw > temp.txt
 set /p path=<temp.txt
 del temp.txt
 
-set "GameDir=X:\SteamLibrary\steamapps\common\VRChat\"
+set "GameDir=F:\SteamLibrary\steamapps\common\VRChat\"
 
-start "" /D %GameDir% "%GameDir%VRChat.exe" "--url=create?roomId=%randomid%&hidden=true&name=BuildAndRun&url=file:///%path%" --enable-debug-gui --enable-sdk-log-levels --enable-udon-debug-logging  --no-vr --watch-worlds
+set /p ClientCount=Amount of Clients to create(leave blank = 1):
+
+if "%ClientCount%"=="" set ClientCount=1
+for /l %%i in (1, 1, %ClientCount%) do (
+	start "" /D %GameDir% "%GameDir%VRChat.exe" "--url=create?roomId=%randomid%&hidden=true&name=BuildAndRun&url=file:///%path%" --enable-debug-gui --enable-sdk-log-levels --enable-udon-debug-logging  --no-vr --watch-worlds
+)
